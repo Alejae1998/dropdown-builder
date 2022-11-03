c55d36c9d6a02292f93bb4fc4f6857208e7365f4
Alejae1998 <alejagr1998@gmail.com>
2022-11-03 15:08:29 -0700

    displaystats and displayslogans working

--- app.js
+++ app.js
@@ -17,22 +17,50 @@ const sloganInput = document.getElementById('name-input');
 let headCounter = 0;
 let bodyCounter = 0;
 let accessoryCounter = 0;
+let slogans = [];
 /* Events */
 headDropdown.addEventListener('change', (e) => {
     const value = e.target.value;
     headImg.src = `./assets/${value}-head.png`;
+    headCounter++;
+    displayStats();
 });
 
 bodyDropdown.addEventListener('change', (e) => {
     const value = e.target.value;
     bodyImg.src = `./assets/${value}-body.png`;
+    bodyCounter++;
+    displayStats();
 });
 
 accessorySelect.addEventListener('change', (e) => {
     const value = e.target.value;
     accessoryImg.src = `./assets/${value}-accessory.png`;
+    accessoryCounter++;
+    displayStats();
+});
+
+addButton.addEventListener('click', () => {
+    slogans.push(sloganInput.value);
+    displaySlogans();
+
+    sloganInput.value = '';
 });
 
 /* Display Functions */
+function displayStats() {
+    headChangeCounter.textContent = `you changed heads ${headCounter} times`;
+    bodyChangeCounter.textContent = `you changed the body ${bodyCounter} times`;
+    accessoryChangeCounter.textContent = `you have changed the accessory ${accessoryCounter} times`;
+}
+
+function displaySlogans() {
+    sloganList.textContent = '';
+    for (let slogan of slogans) {
+        const ul = document.createElement('ul');
+        ul.textContent = slogan;
+        sloganList.append(ul);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
