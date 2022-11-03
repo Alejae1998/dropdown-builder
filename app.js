d4bef1a1f0fcdb81cb68c0ba5115d6da237c1639
Alejae1998 <alejagr1998@gmail.com>
2022-11-03 14:23:53 -0700

    adding more HTML

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 /* Imports */
-
+const headDropdown = document.getElementById('head-select');
+const middleDropdown = document.getElementById('body-select');
+const bottomDropdown = document.getElementById('bottom-dropdown');
+const displaySlogan = document.getElementById('name-list');
 /* Get DOM Elements */
 
 /* State */
 
 /* Events */
-
+headDropdown.addEventListener('change', () => {});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
