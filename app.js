85140401ed824b4d6bf4c9237fc9292a654d7f65
Alejae1998 <alejagr1998@gmail.com>
2022-11-03 14:39:49 -0700

    adding the imports headDropdown working

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 /* Imports */
 const headDropdown = document.getElementById('head-select');
-const middleDropdown = document.getElementById('body-select');
+const headImg = document.getElementById('head-img');
+const headChangeCounter = document.getElementById('head-change-counter');
+const bodyDropdown = document.getElementById('body-select');
+const bodyImg = document.getElementById('body-img');
+const bodyChangeCounter = document.getElementById('body-change-counter');
 const bottomDropdown = document.getElementById('bottom-dropdown');
-const displaySlogan = document.getElementById('name-list');
+const accessoryImg = document.getElementById('accessory-img');
+const accessoryChangeCounter = document.getElementById('accessory-change-counter');
+const sloganList = document.getElementById('name-list');
+const addButton = document.getElementById('name-button');
+const sloganInput = document.getElementById('name-input');
 /* Get DOM Elements */
 
 /* State */
-
+let headCounter = 0;
+let bodyCounter = 0;
+let accessoryCounter = 0;
 /* Events */
-headDropdown.addEventListener('change', () => {});
+headDropdown.addEventListener('change', (e) => {
+    const value = e.target.value;
+    headImg.src = `./assets/${value}-head.png`;
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
